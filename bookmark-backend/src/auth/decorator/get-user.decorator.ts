227de1be22147bd37811c63d288cdba94e984291
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-06-04 12:15:28 +0530

    developed get user using own token

--- bookmark-backend/src/auth/decorator/get-user.decorator.ts
+++ bookmark-backend/src/auth/decorator/get-user.decorator.ts
@@ -2,7 +2,8 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const GetUser = createParamDecorator(
   (data: string | undefined, ctx: ExecutionContext) => {
-    const request : Express.Request = ctx.switchToHttp().getRequest();
-    return request.user;
+    const request = ctx.switchToHttp().getRequest();
+    const user = request.user;
+    return data ? user?.[data] : user;
   },
 );
